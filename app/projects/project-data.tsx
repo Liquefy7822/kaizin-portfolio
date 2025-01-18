37fdb06e3c0af553a3beb62f0b2d0472b76f75a0
Emmanuel Yee <emmanuel_yee_hong_wei@s2022.ssts.edu.sg>
2025-01-18 10:37:45 +0800

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -2,20 +2,20 @@ export interface Project {
   title: string;
   year: number;
   description: string;
-  url: string;
+  url: string; // This will now correspond to internal paths
 }
 
 export const projects: Project[] = [
   {
     title: "Mithril AI",
     year: 2024,
-    description: "Open science AI resarch lab",
-    url: "https://github.com/mithrilai",
+    description: "Open science AI research lab",
+    url: "mithril-ai", // Internal path as a route
   },
   {
     title: "OpenDeepLearning",
     year: 2023,
     description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
+    url: "opendeeplearning", // Internal path as a route
   },
 ];
